30d7e5624ef6003ee128479089314588596c1be2
ozzi- <oz@oz-web.com>
2018-03-07 10:25:44 +0100

    Update StringOp.java
    refactoring

--- PTK/src/helper/StringOp.java
+++ PTK/src/helper/StringOp.java
@@ -8,8 +8,8 @@ import java.util.Date;
 
 public class StringOp {
 	public static String[] splitRecipient(String recipient) {
-		if (recipient.contains(">")) {
-			int markerPos = recipient.indexOf(">");
+		int markerPos = recipient.indexOf(">");
+		if (markerPos!=-1) {
 			return new String[]{recipient.substring(0, markerPos),(recipient.substring(markerPos+1))};
 		}
 		return new String[] {recipient,recipient};
@@ -34,13 +34,15 @@ public class StringOp {
 		String emailAddress = StringOp.splitRecipient(recipient)[0];
 		String name = StringOp.splitRecipient(recipient)[1];
 		String mailEncoded = StringOp.urlEncodeString(emailAddress);
-		String trackingPixelHTML = "<img src=\""+trackingPixelURL+"?pxl=&id="+mailEncoded+"\" style=\"height:1px !important; width:1px !important; border: 0 !important; margin: 0 !important; padding: 0 !important\" width=\"1\" height=\"1\" border=\"0\">";
+		Date date = new Date();		
+		
+		String trackingPixelHTML = "<img src=\""+trackingPixelURL+"?pxl=&id="+mailEncoded+"\" "
+				+ "style=\"height:1px !important; width:1px !important; border: 0 !important; "
+				+ "margin: 0 !important; padding: 0 !important\" width=\"1\" height=\"1\" border=\"0\">";
 
 		cbody = StringOp.injectVar(cbody, name, "%name%");
 		cbody = StringOp.injectVar(cbody, mailEncoded , "%mailaddress%");
 		cbody = StringOp.injectVar(cbody, trackingPixelHTML , "%trackingpixel%");
-		
-		Date date = new Date();		
 		cbody = StringOp.injectVar(cbody, new SimpleDateFormat("yyyy").format(date) , "%yyyy%");
 		cbody = StringOp.injectVar(cbody, new SimpleDateFormat("dd").format(date) , "%dd%");
 		cbody = StringOp.injectVar(cbody, new SimpleDateFormat("MM").format(date) , "%MM%");
